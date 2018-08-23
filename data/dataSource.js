25daa5f5cb608227337819e3e6f7171e24aeeed7
Emmia Silk <contact@emmiasilk.com>
2018-08-23 18:39:11 -0400

    Various changes - Cleaned up code - Moved data and scripts into subfolders - System for loading data source files - Refactored out card generation - Added support for script parameters

---
+++ data/dataSource.js
@@ -0,0 +1,110 @@
+var charData = {
+  // Metadata
+  "definition": {
+    "stats": {
+      0: {
+        "str": "STR",
+        "con": "CON",
+        "dex": "DEX",
+      },
+      1: {
+        "int": "INT",
+        "wis": "WIS",
+        "cha": "CHA",
+      },
+    }
+  },
+
+  // Characters list
+  "characters": {
+    // Deaglan
+    "deaglan": {
+      "name": "Deaglan",
+      "title": "Silly Sneaky Scout",
+      "class": "Wood-Elf Rogue",
+      "icon": "images/deaglan.png",
+      "stats": {
+        "str": 10, "con": 15, "dex": 20,
+        "int": 19, "wis": 12, "cha": 12
+      }
+    },
+    // // Laura
+    // "laura": {
+    //   "name": "Laura Lightwind",
+    //   "title": "Amazing Magician",
+    //   "class": "Human Fighter",
+    //   "icon": "images/laura.png",
+    //   "stats": {
+    //     "str": 13, "con":  8, "dex": 16,
+    //     "int": 13, "wis": 10, "cha": 15
+    //   }
+    // },
+    // Medb
+    "medb": {
+      "name": "Medb",
+      "title": "Maudlin Mage",
+      "class": "Human Warlock",
+      "icon": "images/medb.png",
+      "stats": {
+        "str":  8, "con": 12, "dex": 15,
+        "int": 10, "wis": 13, "cha": 18
+      }
+    },
+    // Menny
+    "menny": {
+      "name": "Menny",
+      "title": "Klutzy Transformer",
+      "class": "Gnome Druid",
+      "icon": "images/menny.png",
+      "stats": {
+        "str": 10, "con": 18, "dex":  9,
+        "int": 13, "wis": 16, "cha": 13
+      }
+    },
+    // // Benjamin
+    // "ben": {
+    //   "name": "Benjamin",
+    //   "title": "MD, Blunt Force Trauma",
+    //   "class": "Arakocra Monk",
+    //   "icon": "images/ben.png",
+    //   "stats": {
+    //     "str":  8, "con": 16, "dex": 18,
+    //     "int":  8, "wis": 16, "cha":  8
+    //   }
+    // },
+    // Ed
+    "ed": {
+      "name": "Ed",
+      "title": "Lord of All The Darkness",
+      "class": "Tiefling Bard",
+      "icon": "images/ed.png",
+      "stats": {
+        "str": 16, "con": 11, "dex": 13,
+        "int": 12, "wis": 13, "cha": 19
+      }
+    },
+    // // Siebolt
+    // "siebolt": {
+    //   "name": "Siebolt",
+    //   "title": "Loyal Soldier",
+    //   "class": "Fighter",
+    //   "icon": "images/siebolt.png",
+    //   "stats": {
+    //     "str": 16, "con": 15, "dex": 13,
+    //     "int": 10, "wis": 14, "cha":  9
+    //   }
+    // },
+    // Beautiful Assistant
+    "beautifulassistant": {
+      "name": "Beautiful Assistant",
+      "title": "Resident Fuzzball",
+      "class": "Stoat",
+      "icon": "images/beautifulassistant.png",
+      "stats": {
+        "str":  3, "con":  8, "dex": 16,
+        "int":  2, "wis": 12, "cha":  3
+      }
+    },
+
+  }
+};
---
+++ scripts/script.js
@@ -0,0 +1,107 @@
+"use strict";
+
+function nextCard(container) {
+  let oldCard = container.children().first();
+  let newCard = oldCard.next();
+
+  // Unhide next card
+  unhide(newCard);
+
+  // Remove old animation classes
+  newCard.removeClass('anim-card-exit');
+  oldCard.removeClass('anim-card-enter');
+
+  // Set card animation
+  oldCard.addClass('anim-card-exit');
+  newCard.addClass('anim-card-enter');
+
+  // Move old card to bottom of stack
+  container.append(oldCard);
+
+}
+
+function unhide(element) {
+  element.removeClass('hidden');
+}
+function hide(element) {
+  element.addClass('hidden');
+}
+
+/* Main function */
+function run(container, dataSource) {
+  let characterList = dataSource.characters;
+  // render character cards
+  for(let key in characterList)
+  {
+    let id = 'characterCard--'+key;
+    let data = characterList[key];
+    container.append( cardGenerator.renderCard(id, data, dataSource.definition) );
+  }
+}
+
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+$(document).ready(function() {
+  // PARAMS
+  let params = {};
+  let query = "";
+  // Source file
+  query = getUrlParameter("datasource");
+  if(!query) {
+    params.source = "data/dataSource.js";
+  }
+  else {
+    params.source = query;
+  }
+  // Manual
+  query = getUrlParameter("manual");
+  if(query && query != "no" && query != "false") {
+    params.auto = false;
+  }
+  else {
+    params.auto = true;
+  }
+
+
+  // Create script
+  let script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.async = true;
+  script.src = params.source;
+  // Append
+  document.head.appendChild(script);
+  script.addEventListener('load', function() {
+    // Generate cards
+    var cardContainer = $('#cardContainer')
+    run(cardContainer, charData);
+    // Hide cards that should be hidden
+    let firstCard = cardContainer.children().first();
+    hide(cardContainer.children());
+    unhide(firstCard);
+
+    if(params.auto) {
+      setInterval(nextCard, 5000, cardContainer);
+    }
+    else {
+      cardContainer.click(function() {
+        nextCard(cardContainer);
+      })
+    }
+  });
+
+
+
+});
